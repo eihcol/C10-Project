ffcd734d6994a7735d505a2091a8212e40c0bc58
eihcol <88992983+eihcol@users.noreply.github.com>
2021-09-09 09:41:06 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,32 +2,34 @@ var shipImg;
 var ship;
 var seaImg;
 var sea;
+
 function preload(){
-seaImg = loadImage("sea.png");
-shipImg = loadImage("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+ seaImg = loadImage("sea.png");
+ shipImg = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 }
 
 function setup(){
   createCanvas(400,400);
   
-  sea = createSprite(200,180,400,30);
-  sea.addImage("seaImg");
+  sea = createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.scale = 0.3;
 
-  ship = createSprite(50,160,20,50);
-  ship.addImage("shipImg");
+  ship = createSprite(200,200,30,30);
+  ship.addAnimation("movingship",shipImg);
   edges = createEdgeSprites();
-  ship.scale = 0.5;
-  ship.x = 50;
+  ship.scale = 0.2;
+  
   
 
 }
 
 function draw() {
   background("blue");
-  sea.velocityX = -5;
+  sea.velocityX = 5;
 
-  if(sea.x < 0 ){
-    sea.x = sea.width / 2;
+  if(sea.x < 400 ){
+    sea.x = sea.width / 8;
   }
 
   drawSprites();
